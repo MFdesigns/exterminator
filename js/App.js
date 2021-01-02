81ad25acd22dcc5a38177217d3030c175e8cd9cc
MFdesigns <michel.faeh@stafag.ch>
2021-01-02 17:12:38 +0100

    Added page navigation

--- js/App.js
+++ js/App.js
@@ -457,3 +457,31 @@ disasmOutput.addEventListener('click', (event) => {
         App.toggleBreakpoint(id);
     }
 });
+
+const nav = document.getElementsByTagName('nav')[0];
+nav.addEventListener('click', (event) => {
+    const closest = event.target.closest('.nav__btn');
+    if (closest) {
+        const navs = document.getElementsByClassName('nav__btn');
+        const pages = document.getElementsByClassName('page');
+        const { page } = closest.dataset;
+
+        for (let i = 0; i < pages.length; i++) {
+            const pageElem = pages[i];
+            if (pageElem.dataset.page === page) {
+                pageElem.classList.toggle('page--active', true);
+            } else {
+                pageElem.classList.toggle('page--active', false);
+            }
+        }
+
+        for (let i = 0; i < navs.length; i++) {
+            const navElem = navs[i];
+            if (navElem.dataset.page === page) {
+                navElem.classList.toggle('nav__btn--active', true);
+            } else {
+                navElem.classList.toggle('nav__btn--active', false);
+            }
+        }
+    }
+});
