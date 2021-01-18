aa9940d22e6470b71e755a257f97eaf3296a1f0b
MFdesigns <michel.faeh@stafag.ch>
2021-01-18 21:31:30 +0100

    Updated to new protocol

--- js/App.js
+++ js/App.js
@@ -27,6 +27,7 @@ const DebugOperation = {
     DBG_RUN_APP: 0xE0,
     DBG_NEXT_INSTR: 0xE1,
     DBG_CONTINUE_: 0xE2,
+    DBG_STOP_EXE: 0xE3,
     DBG_GET_REGS: 0x10,
     DBG_ERROR: 0xEE,
     DBG_EXE_FIN: 0xFF,
@@ -48,6 +49,60 @@ const ErrorCodes = {
     0x6: 'BREAKPOINT_NOT_EXISTING',
 }
 
+/* HTML Elements */
+const regTableBody = document.getElementsByClassName('reg-table-body')[0];
+const nextBtn = document.getElementsByClassName('next-btn')[0];
+const contBtn = document.getElementsByClassName('cont-btn')[0];
+const startBtn = document.getElementsByClassName('start-btn')[0];
+const stopBtn = document.getElementsByClassName('stop-btn')[0];
+const closeDbgSessBtn = document.getElementsByClassName('close-btn')[0];
+const fileUploadBtn = document.getElementsByClassName('upload-btn')[0];
+const disasmOutput = document.getElementsByClassName('disasm-output')[0];
+const nav = document.getElementsByTagName('nav')[0];
+
+const UIState = {
+    CLOSED_SES: 0,
+    OPEN_SESS: 1,
+    APP_RUNNING: 2,
+}
+
+/**
+ * Sets the toolbar UI state
+ * @param {ToolbarState} state
+ */
+function setUIState(state) {
+    switch (state) {
+        case UIState.CLOSED_SES:
+            closeDbgSessBtn.disabled = true;
+            fileUploadBtn.disabled = true;
+            startBtn.disabled = true;
+            stopBtn.disabled = true;
+            nextBtn.disabled = true;
+            contBtn.disabled = true;
+            break;
+        case UIState.OPEN_SESS:
+            closeDbgSessBtn.disabled = false;
+            fileUploadBtn.disabled = false;
+            startBtn.disabled = false;
+            stopBtn.disabled = true;
+            nextBtn.disabled = true;
+            contBtn.disabled = true;
+
+            if (Dbg.CurrentInstrElem) {
+                Dbg.CurrentInstrElem.classList.toggle('asm-line--active', false);
+            }
+            break;
+        case UIState.APP_RUNNING:
+            closeDbgSessBtn.disabled = false;
+            fileUploadBtn.disabled = false;
+            startBtn.disabled = true;
+            stopBtn.disabled = false;
+            nextBtn.disabled = false;
+            contBtn.disabled = false;
+            break;
+    }
+}
+
 /**
  * Outputs a message to the debug console
  * @param {String} msg
@@ -57,7 +112,114 @@ function debugConsole(msg) {
     dbgConsole.value += `[Debug] ${msg}\n`;
 }
 
-class Application {
+/**
+ * Formats an integer to a 64-bit address string
+ * @param {BigInt|Number} int
+ * @return {String}
+ */
+function formatVAddr(int) {
+    return `0x${int.toString(16).toUpperCase().padStart(16, '0')}`;
+}
+
+/**
+ * Formats a decimal register id to the corresponding register name
+ * @param {Number} id
+ * @return {String}
+ */
+function formatRegId(id) {
+    let regName = '';
+    if (id === 1) {
+        regName = 'ip';
+    } else if (id === 2) {
+        regName = 'sp';
+    } else if (id === 3) {
+        regName = 'bp';
+    } else if (id === 4) {
+        regName = 'fl';
+    } else if (id >= 0x5 && id <= 0x14) {
+        regName = `r${(id - 0x5).toString(10)}`;
+    } else if (id >= 0x15 && id <= 0x24) {
+        regName = `f${(id - 0x15).toString(10)}`;
+    }
+    return regName;
+}
+
+/**
+ * Display file info
+ */
+function displayFileInfo() {
+    // Header information
+    const info = document.getElementsByClassName('ux-header-body')[0];
+    const sections = document.getElementsByClassName('ux-sec-table-body')[0];
+    const secNames = document.getElementsByClassName('ux-sec-name-table-body')[0];
+
+    info.innerHTML += `
+    <tr>
+        <td>Magic</td>
+        <td>0x${Dbg.Source.Magic.toString(16).toUpperCase()}</td>
+    </tr>
+    <tr>
+        <td>Version</td>
+        <td>0x${Dbg.Source.Version.toString(16).toUpperCase()}</td>
+    </tr>
+    <tr>
+        <td>Mode</td>
+        <td>0x${Dbg.Source.Mode.toString(16).toUpperCase()}</td>
+    </tr>
+    <tr>
+        <td>Start Address</td>
+        <td>${formatVAddr(Dbg.Source.StartAddr)}</td>
+    </tr>
+    `;
+
+    // Section table
+    Dbg.Source.Sections.forEach((sec) => {
+        sections.innerHTML += `
+        <tr>
+            <td>0x${sec.Type.toString(16).toUpperCase()}</td>
+            <td>0x${sec.Perms.toString(16).toUpperCase()}</td>
+            <td>${formatVAddr(sec.StartAddr)}</td>
+            <td>0x${sec.Size.toString(16).toUpperCase()}</td>
+            <td>${formatVAddr(sec.NameAddr)}</td>
+        </tr>
+        `;
+    });
+
+    // Section name table
+    Dbg.Source.SectionNames.forEach((name) => {
+        secNames.innerHTML += `
+        <tr>
+            <td>${formatVAddr(name.Addr)}</td>
+            <td>0x${name.Size.toString(16).toUpperCase()}</td>
+            <td>${name.Str}</td>
+        </tr>
+        `;
+    });
+}
+
+function displayDisasm(disasm) {
+    const template = document.getElementsByClassName('asm-line-template')[0].content;
+    const frag = document.createDocumentFragment();
+    const maxLineNumberWidth = disasm.length.toString(10).length * 20;
+    const maxAddrLineWidth = disasm[disasm.length - 1].Addr.toString(16).length;
+
+    disasmOutput.innerHTML = '';
+    disasm.forEach((line, i) => {
+        const asmLine = template.cloneNode(true);
+        const addr = line.Addr.toString(16).padStart(maxAddrLineWidth, '0');
+
+        asmLine.querySelector('.asm-line').id = `asm-${line.Addr}`;
+        asmLine.querySelector('.asm-line__line-number').textContent = i + 1;
+        asmLine.querySelector('.asm-line__line-number').style.width = `${maxLineNumberWidth}px`;
+        asmLine.querySelector('.asm-line__address').textContent = `0x${addr}`;
+        asmLine.querySelector('.asm-line__content').textContent = line.Asm;
+        frag.appendChild(asmLine);
+    });
+
+    disasmOutput.appendChild(frag);
+}
+
+class Debugger {
 
     static ResponseMagic = 0x4772c3bc657a6921n;
     static RequestMagic = 0x4772c3bc657a693fn;
@@ -80,17 +242,11 @@ class Application {
         if (!this.#SessOpen) {
             debugConsole('Trying to open debug session...');
             const op = await this.sendOperation(DebugOperation.OPEN_DBG_SESS, null);
-            const res = this.handleResponse(op);
-            if (res) {
-                debugConsole('Successfully opened debug session');
-                this.#SessOpen = true;
-            } else {
-                debugConsole('Failed to open debug session');
-            }
+            this.handleResponse(op);
 
             setTimeout(() => {
                 this.openSession();
-            }, 5000);
+            }, 2000);
         }
     }
 
@@ -112,15 +268,14 @@ class Application {
      */
     async handleFileUpload(file) {
         const fileBuffer = await file.arrayBuffer();
-        App.Source = new SourceFile(new Uint8Array(fileBuffer));
+        Dbg.Source = new SourceFile(new Uint8Array(fileBuffer));
 
-        App.Source.parse();
+        Dbg.Source.parse();
         displayFileInfo();
 
-        App.Disasm = new Disassembler(App.Source);
-        App.Disasm.disassemble();
-        displayDisasm(App.Disasm.Disasm);
-        App.openSession();
+        Dbg.Disasm = new Disassembler(Dbg.Source);
+        Dbg.Disasm.disassemble();
+        displayDisasm(Dbg.Disasm.Disasm);
     }
 
     updateCurrentInstruction() {
@@ -133,6 +288,10 @@ class Application {
         this.CurrentInstrElem.classList.toggle('asm-line--active', true);
     }
 
+    /**
+     * Output array buffer received from UVM
+     * @param {ArrayBuffer} buff
+     */
     consoleOut(buff) {
         const consoleElem = document.getElementsByClassName('console-output')[0];
         const decoder = new TextDecoder('utf8');
@@ -140,7 +299,7 @@ class Application {
     }
 
     /**
-     *
+     * Updates the register display
      * @param {ArrayBuffer} regBuffer
      */
     updateRegisters(regBuffer) {
@@ -204,7 +363,7 @@ class Application {
             }
             const view = new DataView(buff.buffer);
 
-            view.setBigUint64(0, Application.RequestMagic);
+            view.setBigUint64(0, Debugger.RequestMagic);
             view.setUint8(8, op);
 
             fetch('http://127.0.0.1:2001', {
@@ -237,13 +396,12 @@ class Application {
 
         const magic = resView.getBigUint64(0, true);
 
-        if (magic !== Application.ResponseMagic) {
+        if (magic !== Debugger.ResponseMagic) {
             console.error("Response is missing magic");
             return;
         }
 
         const resOp = resView.getUint8(8);
-
 
         switch (resOp) {
             case DebugOperation.DBG_ERROR: {
@@ -252,24 +410,33 @@ class Application {
                 status = false;
             }
                 break;
+            case DebugOperation.DBG_EXE_FIN:
             case DebugOperation.DBG_GET_REGS:
             case DebugOperation.DBG_NEXT_INSTR: {
-                // 0x25 regs * 9 bytes (1 byte reg id + 8 byte reg value) * 8 bytes magic + 1 byte op
-                const regBuffSize = 36 * 9 + 9;
+                const regBuffSize = 333;
                 const regBuffer = res.slice(0, regBuffSize);
                 const consoleBuffer = res.slice(regBuffSize, res.byteLength);
                 this.updateRegisters(regBuffer);
                 this.consoleOut(consoleBuffer);
                 this.updateCurrentInstruction();
+
+                if (resOp === DebugOperation.DBG_EXE_FIN) {
+                    setUIState(UIState.OPEN_SESS);
+                    debugConsole('Finished execution');
+                }
             }
                 break;
             case DebugOperation.DBG_OPEN_DBG_SESS:
-                setToolbarMode(true);
+                setUIState(UIState.OPEN_SESS);
+                debugConsole('Successfully opened debug session');
+                this.#SessOpen = true;
                 break;
             case DebugOperation.DBG_CLOSE_DBG_SESS:
-                setToolbarMode(false);
+                setUIState(UIState.CLOSED_SES);
                 break;
+            case DebugOperation.DBG_RUN_APP:
             case DebugOperation.DBG_CONTINUE_:
+                setUIState(UIState.APP_RUNNING);
                 const regBuffSize = 36 * 9 + 9;
                 const regBuffer = res.slice(0, regBuffSize);
                 const consoleBuffer = res.slice(regBuffSize, res.byteLength);
@@ -278,7 +445,7 @@ class Application {
                 this.updateCurrentInstruction();
                 break;
             default:
-                console.error("Reponse contains unknown operation code");
+                console.error(`Reponse contains unknown operation code: ${resOp}`);
                 status = false;
                 break;
         }
@@ -286,43 +453,11 @@ class Application {
         return status;
     }
 
-    async requestHandshake() {
-        return new Promise((resolve, reject) => {
-            const encoder = new TextEncoder('utf-8');
-            const magic = 0x4772c3bc657a693fn; // Grüezi?
-            const operation = DebugOperation.OPEN_DBG_SESS;
-
-            const filePath = "/tmp/";
-            const filePathSize = filePath.length;
-
-            const buffSize = 8 + 1 + 4 + filePathSize + 4 + App.Source.FileBuffer.length;
-            const buff = new Uint8Array(buffSize);
-            const view = new DataView(buff.buffer);
-
-            view.setBigUint64(0, magic);
-            view.setUint8(8, operation);
-            view.setUint32(9, filePathSize, true);
-            buff.set(encoder.encode(filePath), 13);
-            view.setUint32(13 + filePathSize, App.Source.FileBuffer.length, true);
-            buff.set(App.Source.FileBuffer, 13 + filePathSize + 4);
-
-            fetch('http://127.0.0.1:2001', {
-                method: 'POST',
-                body: buff
-            }).then((res) => {
-                res.arrayBuffer().then((buff) => {
-                    resolve(buff);
-                })
-            }).catch((err) => {
-                reject(err);
-            })
-        });
-    }
 
     /**
-*
-* @param {BigInteger} flagReg
-*/
+    * Updates the flags display
+    * @param {BigInteger} flagReg
+    */
     setFlags(flagReg) {
         const flagsTableBody = document.getElementsByClassName('flags-table-body')[0];
         const clearMask = 1;
@@ -353,162 +488,54 @@ class Application {
         if (this.Breakpoints.includes(bpAddr)) {
             const bpIndex = this.Breakpoints.indexOf(bpAddr);
             this.Breakpoints.splice(bpIndex, 1);
-            this.sendOperation(DebugOperation.REMOVE_BREAKPOINT, buffer);
+            this.sendOperation(DebugOperation.DBG_REMOVE_BREAKPNT, buffer);
         } else {
             this.Breakpoints.push(bpAddr);
-            this.sendOperation(DebugOperation.SET_BREAKPOINT, buffer);
+            this.sendOperation(DebugOperation.DBG_SET_BREAKPNT, buffer);
         }
     }
 }
 
-const App = new Application();
+const Dbg = new Debugger();
 
-const regTableBody = document.getElementsByClassName('reg-table-body')[0];
-const nextBtn = document.getElementsByClassName('next-btn')[0];
-const contBtn = document.getElementsByClassName('cont-btn')[0];
-const stopBtn = document.getElementsByClassName('stop-btn')[0];
-
-function setToolbarMode(state) {
-    if (state) {
-        nextBtn.disabled = false;
-        stopBtn.disabled = false;
-        contBtn.disabled = false;
-    } else {
-        nextBtn.disabled = true;
-        stopBtn.disabled = true;
-        contBtn.disabled = true;
-    }
-}
-
-/**
- * Formats an integer to a 64-bit address string
- * @param {BigInt|Number} int
- * @return {String}
- */
-function formatVAddr(int) {
-    return `0x${int.toString(16).toUpperCase().padStart(16, '0')}`;
-}
-
-/**
- * Formats a decimal register id to the corresponding register name
- * @param {Number} id
- * @return {String}
- */
-function formatRegId(id) {
-    let regName = '';
-    if (id === 1) {
-        regName = 'ip';
-    } else if (id === 2) {
-        regName = 'sp';
-    } else if (id === 3) {
-        regName = 'bp';
-    } else if (id === 4) {
-        regName = 'fl';
-    } else if (id >= 0x5 && id <= 0x14) {
-        regName = `r${(id - 0x5).toString(10)}`;
-    } else if (id >= 0x15 && id <= 0x24) {
-        regName = `f${(id - 0x15).toString(10)}`;
-    }
-    return regName;
-}
-
-function displayFileInfo() {
-    // Header information
-    const info = document.getElementsByClassName('ux-header-body')[0];
-    const sections = document.getElementsByClassName('ux-sec-table-body')[0];
-    const secNames = document.getElementsByClassName('ux-sec-name-table-body')[0];
-
-    info.innerHTML += `
-    <tr>
-        <td>Magic</td>
-        <td>0x${App.Source.Magic.toString(16).toUpperCase()}</td>
-    </tr>
-    <tr>
-        <td>Version</td>
-        <td>0x${App.Source.Version.toString(16).toUpperCase()}</td>
-    </tr>
-    <tr>
-        <td>Mode</td>
-        <td>0x${App.Source.Mode.toString(16).toUpperCase()}</td>
-    </tr>
-    <tr>
-        <td>Start Address</td>
-        <td>${formatVAddr(App.Source.StartAddr)}</td>
-    </tr>
-    `;
-
-    // Section table
-    App.Source.Sections.forEach((sec) => {
-        sections.innerHTML += `
-        <tr>
-            <td>0x${sec.Type.toString(16).toUpperCase()}</td>
-            <td>0x${sec.Perms.toString(16).toUpperCase()}</td>
-            <td>${formatVAddr(sec.StartAddr)}</td>
-            <td>0x${sec.Size.toString(16).toUpperCase()}</td>
-            <td>${formatVAddr(sec.NameAddr)}</td>
-        </tr>
-        `;
-    });
-
-    // Section name table
-    App.Source.SectionNames.forEach((name) => {
-        secNames.innerHTML += `
-        <tr>
-            <td>${formatVAddr(name.Addr)}</td>
-            <td>0x${name.Size.toString(16).toUpperCase()}</td>
-            <td>${name.Str}</td>
-        </tr>
-        `;
-    });
-}
 // =================== //
 //   EVENT LISTENERS
 // =================== //
 
-const disasmOutput = document.getElementsByClassName('disasm-output')[0];
-function displayDisasm(disasm) {
-    const template = document.getElementsByClassName('asm-line-template')[0].content;
-    const frag = document.createDocumentFragment();
-    const maxLineNumberWidth = disasm.length.toString(10).length * 20;
-    const maxAddrLineWidth = disasm[disasm.length - 1].Addr.toString(16).length;
+startBtn.addEventListener('click', async () => {
+    // TODO: Only works if file has been uploaded
+    const fileBuffSize = Dbg.Source.FileBuffer.byteLength;
+    const buffSize = 4 + fileBuffSize;
+    const buff = new Uint8Array(buffSize);
+    const view = new DataView(buff.buffer);
 
-    disasmOutput.innerHTML = '';
-    disasm.forEach((line, i) => {
-        const asmLine = template.cloneNode(true);
-        const addr = line.Addr.toString(16).padStart(maxAddrLineWidth, '0');
+    view.setUint32(0, fileBuffSize, true);
+    buff.set(Dbg.Source.FileBuffer, 4);
 
-        asmLine.querySelector('.asm-line').id = `asm-${line.Addr}`;
-        asmLine.querySelector('.asm-line__line-number').textContent = i + 1;
-        asmLine.querySelector('.asm-line__line-number').style.width = `${maxLineNumberWidth}px`;
-        asmLine.querySelector('.asm-line__address').textContent = `0x${addr}`;
-        asmLine.querySelector('.asm-line__content').textContent = line.Asm;
-        frag.appendChild(asmLine);
-    });
+    const req = await Dbg.sendOperation(DebugOperation.DBG_RUN_APP, buff);
+    Dbg.handleResponse(req);
+});
 
-    disasmOutput.appendChild(frag);
-}
-
-const fileUploadBtn = document.getElementsByClassName('upload-btn')[0];
 fileUploadBtn.addEventListener('change', async () => {
     const file = fileUploadBtn.files[0];
     if (file) {
-        App.handleFileUpload(file);
+        Dbg.handleFileUpload(file);
     }
 });
 
 nextBtn.addEventListener('click', async () => {
-    const regArray = await App.sendOperation(DebugOperation.NEXT_INSTR);
-    App.handleResponse(regArray);
+    const regArray = await Dbg.sendOperation(DebugOperation.DBG_NEXT_INSTR);
+    Dbg.handleResponse(regArray);
 });
 
 stopBtn.addEventListener('click', () => {
-    App.sendOperation(DebugOperation.CLOSE_DBG_SESS);
-    setToolbarMode(false);
+    Dbg.sendOperation(DebugOperation.DBG_STOP_EXE);
+    setUIState(UIState.OPEN_SESS);
 });
 
 contBtn.addEventListener('click', () => {
-    App.sendOperation(DebugOperation.CONTINUE).then((res) => {
-        App.handleResponse(res);
+    Dbg.sendOperation(DebugOperation.DBG_CONTINUE_).then((res) => {
+        Dbg.handleResponse(res);
     });
 });
 
@@ -516,11 +543,10 @@ disasmOutput.addEventListener('click', (event) => {
     if (event.target.classList.contains('asm-line__breakpoint')) {
         const id = event.target.parentNode.id;
         event.target.classList.toggle('asm-line__breakpoint--active');
-        App.toggleBreakpoint(id);
+        Dbg.toggleBreakpoint(id);
     }
 });
 
-const nav = document.getElementsByTagName('nav')[0];
 nav.addEventListener('click', (event) => {
     const closest = event.target.closest('.nav__btn');
     if (closest) {
@@ -548,7 +574,6 @@ nav.addEventListener('click', (event) => {
     }
 });
 
-const closeDbgSessBtn = document.getElementsByClassName('close-btn')[0];
 closeDbgSessBtn.addEventListener('click', () => {
-    App.closeSession();
+    Dbg.closeSession();
 });
