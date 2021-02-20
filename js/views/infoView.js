04a2c5dfba629ae7e9edf8f357b0956daa696b2e
MFdesigns <michel.faeh@stafag.ch>
2021-02-20 20:21:13 +0100

    Added info page layout

--- js/views/infoView.js
+++ js/views/infoView.js
@@ -26,34 +26,85 @@ export class InfoView {
     this.Elem.sections.innerHTML = '';
     this.Elem.secNames.innerHTML = '';
 
+    let modeStr = 'Unkown';
+    switch (src.Mode) {
+      case 0x1:
+        modeStr = 'Release';
+        break;
+      case 0x2:
+        modeStr = 'Debug';
+        break;
+    }
+
     this.Elem.info.innerHTML += `
     <tr>
         <td>Magic</td>
-        <td>0x${src.Magic.toString(16).toUpperCase()}</td>
+        <td class="mono">0x${src.Magic.toString(16).toUpperCase()}</td>
     </tr>
     <tr>
         <td>Version</td>
-        <td>0x${src.Version.toString(16).toUpperCase()}</td>
+        <td class="mono">0x${src.Version.toString(16).toUpperCase()}</td>
     </tr>
     <tr>
         <td>Mode</td>
-        <td>0x${src.Mode.toString(16).toUpperCase()}</td>
+        <td><span class="mono">0x${src.Mode.toString(16).toUpperCase()}</span> (${modeStr})</td>
     </tr>
     <tr>
         <td>Start Address</td>
-        <td>${formatVAddr(src.StartAddr)}</td>
+        <td class="mono">${formatVAddr(src.StartAddr)}</td>
     </tr>
     `;
 
     // Section table
     src.Sections.forEach((sec) => {
+      let typeStr = 'Unknown';
+      switch (sec.Type) {
+        case 0x1:
+          typeStr = 'SEC_NAME_STRINGS';
+          break;
+        case 0x2:
+          typeStr = 'SEC_META_DATA';
+          break;
+        case 0x3:
+          typeStr = 'SEC_DEBUG';
+          break;
+        case 0x4:
+          typeStr = 'SEC_STATIC';
+          break;
+        case 0x5:
+          typeStr = 'SEC_GLOBAL';
+          break;
+        case 0x6:
+          typeStr = 'SEC_CODE';
+          break;
+      }
+
+      let permStr = '';
+      if ((sec.Perms & 0b10000000) > 0) {
+        permStr += 'r';
+      } else {
+        permStr += '-';
+      }
+
+      if ((sec.Perms & 0b01000000) > 0) {
+        permStr += 'w';
+      } else {
+        permStr += '-';
+      }
+
+      if ((sec.Perms & 0b00100000) > 0) {
+        permStr += 'x';
+      } else {
+        permStr += '-';
+      }
+
       this.Elem.sections.innerHTML += `
         <tr>
-            <td>0x${sec.Type.toString(16).toUpperCase()}</td>
-            <td>0x${sec.Perms.toString(16).toUpperCase()}</td>
-            <td>${formatVAddr(sec.StartAddr)}</td>
-            <td>0x${sec.Size.toString(16).toUpperCase()}</td>
-            <td>${formatVAddr(sec.NameAddr)}</td>
+            <td><span class="mono">0x${sec.Type.toString(16).toUpperCase()}</span> (${typeStr})</td>
+            <td class="mono">0x${sec.Perms.toString(16).toUpperCase()} [${permStr}]</td>
+            <td class="mono">${formatVAddr(sec.StartAddr)}</td>
+            <td class="mono">0x${sec.Size.toString(16).toUpperCase()}</td>
+            <td class="mono">${formatVAddr(sec.NameAddr)}</td>
         </tr>
         `;
     });
@@ -62,8 +113,8 @@ export class InfoView {
     src.SectionNames.forEach((name) => {
       this.Elem.secNames.innerHTML += `
         <tr>
-            <td>${formatVAddr(name.Addr)}</td>
-            <td>0x${name.Size.toString(16).toUpperCase()}</td>
+            <td class="mono">${formatVAddr(name.Addr)}</td>
+            <td class="mono">0x${name.Size.toString(16).toUpperCase()}</td>
             <td>${name.Str}</td>
         </tr>
         `;
